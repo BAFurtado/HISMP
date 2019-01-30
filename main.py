fcbb3be4957ae47610aea08e42ee20b562e2aaa5
furtadobb <furtadobb@gmail.com>
2019-01-30 17:28:35 -0200

    Different saving. Different p

--- main.py
+++ main.py
@@ -31,7 +31,8 @@ def main(males, females):
         [x.send_msg() for x in males]
         [x.send_msg() for x in females]
         current = sum([1 for each in [males, females] for x in each if x.my_partner == None])
-        not_msg = sum([1 for each in [males, females] for x in each if (x.status == True) and (x.my_partner == None)
+        not_msg = sum([1 for each in [males, females] for x in each if (x.status == True)
+                       and (x.my_partner == None)
                        and (x.messaged == False)])
         print('Still single: ', current)
         print('Not messaged: ', not_msg)
@@ -40,14 +41,22 @@ def main(males, females):
     return males, females
 
 
-def gen_groups(group1, group2, alpha):
+def gen_groups(group1, group2, alpha, beta=1):
     # Generate groups with a percentage (alpha) of active status (actively sends messages)
     m1, f1 = [], []
     for i in range(group1):
-        m1.append(Male(i, np.random.choice([True, False], p=[alpha, 1 - alpha])))
+        m1.append(Male(i, np.random.choice([True, False], p=[beta, 1 - beta])))
     for j in range(group2):
         f1.append(Female(j, np.random.choice([True, False], p=[1 - alpha, alpha])))
     return m1, f1
+
+
+def calculate_energy(gr1, gr2, pp, homme, femme):
+    res_f = np.mean([femme[i].energy() for i in range(len(femme))])
+    res_m = np.mean([homme[i].energy() for i in range(len(homme))])
+    with open('energy.csv', 'a') as fl:
+        fl.write('{};{};{};{};{}'.format(gr1, gr2, pp, res_m, res_f))
+    return res_m, res_f
 
 
 if __name__ == '__main__':
@@ -57,5 +66,4 @@ if __name__ == '__main__':
     m, f = gen_groups(g1, g2, p)
     m, f = main(m, f)
     # 3. Print Energy
-    print('Final mean energy females {}'.format(np.mean([f[i].energy() for i in range(len(f))])))
-    print('Final mean energy males {}'.format(np.mean([m[i].energy() for i in range(len(m))])))
+    print('Final mean energy males: {}, females: {}'.format(calculate_energy(g1, g2, p, m, f)))
--- repeat.py
+++ repeat.py
@@ -4,7 +4,7 @@ import main
 from plotting import plot
 
 
-def generate(n, alpha):
+def generate(n, alpha, beta=1):
     d = [x * 100 for x in range(1, 21)]
     d.insert(0, 1)
     d.insert(1, 50)
@@ -16,20 +16,20 @@ def generate(n, alpha):
     
     for k in range(n):
         for j in range(len(d)):
-            males, females = main.gen_groups(1000, d[j], alpha)
+            males, females = main.gen_groups(1000, d[j], alpha, beta)
             males, females = main.main(males, females)
             m_matrix[k, j] = np.mean([males[i].energy() for i in range(len(males))])
             f_matrix[k, j] = np.mean([females[i].energy() for i in range(len(females))])
-            print('One more run done ...!', j)
+            print('One more run done ...! {} Repetition {}'.format(j, k))
         print('One more repetition done ...!', k)
-        np.savetxt('saved_data/m_{}.txt'.format(k), m_matrix)
-        np.savetxt('saved_data/f_{}.txt'.format(k), f_matrix)
 
     return m_matrix, f_matrix, d
 
 
 if __name__ == '__main__':
-    number = 5  # Number of repetitions
+    number = 3  # Number of repetitions
     for p in np.linspace(1, 0, 11):
         m, f, D = generate(number, p)
+        np.savetxt('saved_data/m_{}.txt'.format(p), m)
+        np.savetxt('saved_data/f_{}.txt'.format(p), f)
         plot(m, f, D, p, False)
