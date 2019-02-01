4bcc0ba73e9a4a4e20bba5d839ad6061df639cf5
furtadobb <furtadobb@gmail.com>
2019-02-01 10:16:21 -0200

    alpha, beta

--- repeat.py
+++ repeat.py
@@ -29,7 +29,15 @@ def generate(n, alpha, beta=1):
 if __name__ == '__main__':
     number = 3  # Number of repetitions
     for p in np.linspace(1, 0, 11):
-        m, f, D = generate(number, p)
-        np.savetxt('saved_data/m_{}.txt'.format(p), m)
-        np.savetxt('saved_data/f_{}.txt'.format(p), f)
+        # Two alternatives. b = 1, Beta, then males are all active
+        # b = 1 - p, the full probability of being active is 1
+        b = 1
+        m, f, D = generate(number, p, b)
+        np.savetxt('saved_data/m_{}_{}.txt'.format(p, b), m)
+        np.savetxt('saved_data/f_{}_{}.txt'.format(p, b), f)
+        plot(m, f, D, p, False)
+        b = 1 - p
+        m, f, D = generate(number, p, b)
+        np.savetxt('saved_data/m_{}_{}.txt'.format(p, b), m)
+        np.savetxt('saved_data/f_{}_{}.txt'.format(p, b), f)
         plot(m, f, D, p, False)
